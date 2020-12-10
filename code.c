6fd957e6f8e6511dfa3d593dbd51880c4bfe68ab
bionic-beaver-ikt <protopoper@mail.ru>
2020-12-10 21:51:00 +0800

    third commit

--- code.c
+++ code.c
@@ -1,16 +1,31 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define THERM_PORT PORTD
+#define THERM_DDR DDRD
+#define THERM_PIN PIND
+#define THERM_DQ PD2
+
+#define THERM_INPUT_MODE() THERM_DDR&=~(1<<THERM_DQ)
+#define THERM_OUTPUT_MODE() THERM_DDR|=(1<<THERM_DQ)
+#define THERM_LOW() THERM_PORT&=~(1<<THERM_DQ)
+
+int main (void)
+{
 	THERM_LOW();
 	THERM_OUTPUT_MODE();
 	_delay_us(480);
 	THERM_INPUT_MODE();
 	_delay_us(60);
-	i=(THERM_PIN & (1<<THERM_DQ));
+	int bit=(THERM_PIN & (1<<THERM_DQ));
 	_delay_us(420);
 	
 	THERM_LOW();
 	THERM_INPUT_MODE();
 	while (THERM_PIN&(1<<THERM_DQ)) {};
 	_delay_us(480);
-	for (i=0; i<480; i++)
+	for (int i=0; i<480; i++)
 	{
 		if(THERM_PIN&(1<<THERM_DQ))
 		{
@@ -22,9 +37,9 @@
 			_delay_us(190);
 			while (THERM_PIN&(1<<THERM_DQ)) {};
 			int command[32];
-			for (j=0; j<32; j++)
+			for (int j=0; j<32; j++)
 			{
-				for (k=0; k<120; k++)
+				for (int k=0; k<120; k++)
 				{
 					if(THERM_PIN&(1<<THERM_DQ))
 					{
@@ -39,9 +54,9 @@
 	}
 
 
-	}
+	
 	_delay_us(1);
-	}
+    
 
 	
 	THERM_LOW();
@@ -58,3 +73,5 @@
 	_delay_us(14);
 	if(THERM_PIN&(1<<THERM_DQ)) bit=1;
 	_delay_us(45);
+    
+    }
