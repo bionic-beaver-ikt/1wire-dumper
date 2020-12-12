9928e26080554c717f7a18b3731d933b12589ea1
bionic-beaver-ikt <protopoper@mail.ru>
2020-12-12 13:41:03 +0800

    4th commit

--- code.c
+++ code.c
@@ -1,11 +1,14 @@
-#define F_CPU 16000000UL
+#define F_CPU 12000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
 #define THERM_PORT PORTD
 #define THERM_DDR DDRD
 #define THERM_PIN PIND
-#define THERM_DQ PD2
+#define THERM_DQ PD3
+#define LED0 PD0
+#define LED1 PD1
+#define LED2 PD2
 
 #define THERM_INPUT_MODE() THERM_DDR&=~(1<<THERM_DQ)
 #define THERM_OUTPUT_MODE() THERM_DDR|=(1<<THERM_DQ)
@@ -13,6 +16,9 @@
 
 int main (void)
 {
+    int led0_bit=0;
+    int led1_bit=0;
+    
 	THERM_LOW();
 	THERM_OUTPUT_MODE();
 	_delay_us(480);
@@ -20,11 +26,42 @@ int main (void)
 	_delay_us(60);
 	int bit=(THERM_PIN & (1<<THERM_DQ));
 	_delay_us(420);
-	
+
+void led0_blink()
+{
+    if (led0_bit==0)
+    {
+        THERM_DDR&=~(1<<THERM_DQ);
+        led0_bit=1;
+    }
+    else
+    {
+        THERM_DDR|=(1<<THERM_DQ);
+        led0_bit=0;
+    }
+}
+
+void led1_blink()
+{
+    if (led1_bit==0)
+    {
+        THERM_DDR&=~(1<<THERM_DQ);
+        led1_bit=1;
+    }
+    else
+    {
+        THERM_DDR|=(1<<THERM_DQ);
+        led1_bit=0;
+    }
+}
+    
+void init_receive()
+{
 	THERM_LOW();
 	THERM_INPUT_MODE();
 	while (THERM_PIN&(1<<THERM_DQ)) {};
 	_delay_us(480);
+    
 	for (int i=0; i<480; i++)
 	{
 		if(THERM_PIN&(1<<THERM_DQ))
@@ -52,13 +89,12 @@ int main (void)
 		}
 	_delay_us(1);
 	}
+}
 
-
-	
 	_delay_us(1);
     
-
-	
+void transmit(void)
+{
 	THERM_LOW();
 	THERM_OUTPUT_MODE();
 	_delay_us(1);
@@ -73,5 +109,11 @@ int main (void)
 	_delay_us(14);
 	if(THERM_PIN&(1<<THERM_DQ)) bit=1;
 	_delay_us(45);
-    
+}    
+    while (1)
+    {
+     init_receive();   
     }
+}
+
+    
