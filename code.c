757fffdb543aa8e652da39cac4e57596322f21a3
bionic-beaver-ikt <protopoper@mail.ru>
2021-01-11 23:09:57 +0800

    transmit pseudo-ID

--- code.c
+++ code.c
@@ -9,11 +9,6 @@
 #define THERM_DQ PB5
 
 #define LED0 PD0
-#define LED1 PD1
-#define LED2 PD2
-
-#define Button1 PD2
-#define Button2 PD3
 
 #define DDI_D PD6
 #define DDI_C PD7
@@ -28,19 +23,17 @@
 #define THERM_OUTPUT_MODE() THERM_DDR|=(1<<THERM_DQ)
 #define THERM_LOW() THERM_PORT&=~(1<<THERM_DQ)
 
-int num_of_sign=32;
-int command[32];//={99,99,99,99,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,99};
+int num_of_sign=4;
+int command[4]={0,0,0,0}; //={99,99,99,99,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,99};
 //command[]={2};
 
 void show()
 {
      while (num_of_sign>=0)
-	 //for (int times=0; times<128; times--)
 	{
 		PORTD&=~((1<<DDI1)|(1<<DDI2)|(1<<DDI3)|(1<<DDI4));
 		PORTB&=~((1<<DDI_A)|(1<<DDI_B));
 		PORTD&=~((1<<DDI_C)|(1<<DDI_D));
-        //sign (num_of_sign/10);
 		if (((num_of_sign/10)%16/8)==1) PORTD|=(1<<DDI_D);
 		if (((num_of_sign/10)%8/4)==1) PORTD|=(1<<DDI_C);
 		if (((num_of_sign/10)%4/2)==1) PORTB|=(1<<DDI_B);
@@ -50,7 +43,6 @@ void show()
         PORTD&=~((1<<DDI1)|(1<<DDI2)|(1<<DDI3)|(1<<DDI4));
 		PORTB&=~((1<<DDI_A)|(1<<DDI_B));
 		PORTD&=~((1<<DDI_C)|(1<<DDI_D));
-        //sign (num_of_sign%10);
 		if (((num_of_sign%10)%16/8)==1) PORTD|=(1<<DDI_D);
 		if (((num_of_sign%10)%8/4)==1) PORTD|=(1<<DDI_C);
 		if (((num_of_sign%10)%4/2)==1) PORTB|=(1<<DDI_B);
@@ -64,7 +56,6 @@ void show()
 		if (((command[num_of_sign]/10)%8/4)==1) PORTD|=(1<<DDI_C);
 		if (((command[num_of_sign]/10)%4/2)==1) PORTB|=(1<<DDI_B);
 		if (((command[num_of_sign]/10)%2)==1) PORTB|=(1<<DDI_A);
-        //command[num_of_sign]/10;
 		PORTD|=(1<<DDI3);
         _delay_ms(2);
         PORTD&=~((1<<DDI1)|(1<<DDI2)|(1<<DDI3)|(1<<DDI4));
@@ -76,7 +67,6 @@ void show()
 		if (((command[num_of_sign]%10)%2)==1) PORTB|=(1<<DDI_A);
 		PORTD|=(1<<DDI4);
 		_delay_ms(2);
-        //command[num_of_sign]%10;
 	}
 }
 
@@ -84,31 +74,16 @@ ISR (TIMER1_COMPA_vect)
 {
 	cli();
 	DDRD|=((1<<DDI1)|(1<<DDI2)|(1<<DDI3)|(1<<DDI4)); //Порты сегментов на вывод
-        //PORTD&=~((1<<DDI1)|(1<<DDI2)); //|(1<<DDI3)|(1<<DDI4));
     PORTD|=(1<<DDI3)|(1<<DDI4)|(1<<DDI1)|(1<<DDI2); //Порты сегментов на VCC (цифры погашены)
-        
     DDRB|=((1<<DDI_A)|(1<<DDI_B)); //Порты данных на вывод
     DDRD|=((1<<DDI_C)|(1<<DDI_D)); //Порты данных на вывод
     PORTB&=~((1<<DDI_A)|(1<<DDI_B));
     PORTD&=~((1<<DDI_C)|(1<<DDI_D)); //0
 	num_of_sign--;
-	if(num_of_sign<0)
-    {
-		sei();
-        show();
-		 cli();
-		 //num_of_sign=32;
-		 //_delay_ms(500);
-    }
-    else
-    {
-        sei();
-        show();
-    }
-	//_delay_ms(2000);
-}        
-
-
+	sei();
+    show();
+	if(num_of_sign<0) cli();
+}
 
 int main (void)
 {
@@ -119,32 +94,8 @@ int main (void)
 	 //OCR1AH = 0x57;
      OCR1AL = 0x1B;
      TCCR1B |= (1<<CS12);
-     
-    /*DDRD|=((1<<DDI1)|(1<<DDI2)|(1<<DDI3)|(1<<DDI4));
-        PORTD&=~((1<<DDI1)|(1<<DDI2)|(1<<DDI3)|(1<<DDI4));
-        
-        DDRD|=((1<<DDI_A)|(1<<DDI_B));
-        DDRB|=((1<<DDI_C)|(1<<DDI_D));
-        PORTD&=~((1<<DDI_A)|(1<<DDI_B));
-        PORTB&=~((1<<DDI_C)|(1<<DDI_D));
-        
-        _delay_ms(4800);*/
-        
     int led0_bit=0;
-    int led1_bit=0;
-    
-	
-void hren()
-{
-	THERM_LOW();
-	THERM_OUTPUT_MODE();
-	_delay_us(480);
-	THERM_INPUT_MODE();
-	_delay_us(60);
-	int bit=(THERM_PIN & (1<<THERM_DQ));
-	_delay_us(420);
-}
-    
+
 void led0_blink()
 {
     if (led0_bit==0)
@@ -159,59 +110,62 @@ void led0_blink()
     }
 }
 
-void led1_blink()
+int init_receive()
 {
-    if (led1_bit==0)
-    {
-        THERM_DDR&=~(1<<THERM_DQ);
-        led1_bit=1;
-    }
-    else
-    {
-        THERM_DDR|=(1<<THERM_DQ);
-        led1_bit=0;
-    }
-}
-    
-void init_receive()
-{
+	int code = 0;
 	THERM_INPUT_MODE();
 	_delay_us(10);
 	while (THERM_PIN&(1<<THERM_DQ)) {}; //1
 	_delay_us(470); //2
-    
+
 	for (int i=0; i<480; i++) //3
 	{
 		if(THERM_PIN&(1<<THERM_DQ)) //3a
-		{    
+		{
 			i=480;
 			_delay_us(55); //3b
 			THERM_OUTPUT_MODE();
 			_delay_us(235); //c
 			THERM_INPUT_MODE();
 			_delay_us(1); //(190)
-            
-			for (int j=31; j>=0; j--)
+
+			for (int j=0; j<8; j++)
 			{
-				command[j] = 0;
 				while (THERM_PIN&(1<<THERM_DQ)) {};
 				for (int kl=0; kl<100; kl++)
 				{
 					if (THERM_PIN&(1<<THERM_DQ))
                     {
-                        command[j] = kl;
-						/*if (kl<20)
-                        {
-                            command[j] = kl+1;
-                            //PORTB&=~(1<<PB4);
-                            //PORTB|=(1<<PB3);
-                        }
-                        else
-                        {
-                            command[j] = 0;
-                            //PORTB&=~(1<<PB3);
-                            //PORTB|=(1<<PB4);
-                        }*/
+						if (kl < 4)
+						{
+							switch (j)
+							{
+								case 0:
+								code += 1;
+								break;
+								case 1:
+								code += 2;
+								break;
+								case 2:
+								code += 4;
+								break;
+								case 3:
+								code += 8;
+								break;
+								case 4:
+								code += 16;
+								break;
+								case 5:
+								code += 32;
+								break;
+								case 6:
+								code +=64;
+								break;
+								case 7:
+								code += 128;
+								break;
+							}
+						}
 						kl=101;
 					}
                     else _delay_us(1);
@@ -220,88 +174,52 @@ void init_receive()
 		}
 	_delay_us(1);
 	}
+return code;
 }
 
-	//_delay_us(1);
-    
-void transmit()
+void transmit(int byte)
 {
-	THERM_LOW();
-	THERM_OUTPUT_MODE();
-	_delay_us(1);
-	//if(bit) THERM_INPUT_MODE();
-	_delay_us(60);
 	THERM_INPUT_MODE();
-	
-	THERM_LOW();
-	THERM_OUTPUT_MODE();
-	_delay_us(1);
-	THERM_INPUT_MODE();
-	_delay_us(14);
-	//if(THERM_PIN&(1<<THERM_DQ)) bit=1;
-	_delay_us(45);
-}    
-
-/*void sign(int num)
-{
-
-}*/
-
+	uint8_t i=8;
+	while(i--){
+		while (THERM_PIN&(1<<THERM_DQ)) {};
+		if (!(byte&1)) THERM_OUTPUT_MODE();
+		_delay_us(16);
+		THERM_INPUT_MODE();
+		byte>>=1;
+	}
+}
 
     while (1)
     {
 		cli();
-        num_of_sign=32;
+        num_of_sign=8;
         THERM_LOW();
-		
+
 		PORTD&=~((1<<DDI1)|(1<<DDI2)|(1<<DDI3)|(1<<DDI4));
 		PORTB&=~((1<<DDI_A)|(1<<DDI_B));
 		PORTD&=~((1<<DDI_C)|(1<<DDI_D));
-        
-        DDRB|=(1<<PB3);  
+
+        DDRB|=(1<<PB3);
         PORTB|=(1<<PB3);
-		_delay_ms(10000); 
+		_delay_ms(1000);
 		PORTB&=~(1<<PB3);
-		init_receive();
+		if (init_receive() == 0x33)
+		{
+			command[3]=73;
+			transmit(0x12);
+			transmit(0x34);
+			transmit(0x56);
+			transmit(0x78);
+			transmit(0x9A);
+			transmit(0xBC);
+			transmit(0xDE);
+			transmit(0xF0);
+		}
 		sei();
 		_delay_ms(1000);
 		cli();
 		PORTB|=(1<<PB3);
-		//PORTD|=(1<<DDI_D);
-		//PORTD|=((1<<DDI1)|(1<<DDI2)|(1<<DDI3)|(1<<DDI4));
 		_delay_ms(1000);
-		
-     //init_receive();
-        //DDRD&=~((1<<DDI1)|(1<<DDI2)|(1<<DDI3)|(1<<DDI4));
-        
-		
-		//_delay_ms(500);
-		//sei();
-		//_delay_ms(2000);
-		
-        /*PORTD|=(1<<DDI_D); //8
-        _delay_ms(500);
-        PORTD&=~(1<<DDI1);
-        PORTB|=(1<<DDI_A); //9
-        _delay_ms(500);
-        PORTB&=~(1<<DDI_A); //10
-        PORTB|=(1<<DDI_B);
-        _delay_ms(500);
-        PORTB|=(1<<DDI_A); //11
-        _delay_ms(500);
-        PORTB&=~(1<<DDI_A);
-        PORTB&=~(1<<DDI_B);
-        PORTD|=(1<<DDI_C); //12
-        _delay_ms(500);
-        PORTB|=(1<<DDI_A); //13
-        _delay_ms(500);
-        PORTB&=~(1<<DDI_A); //14
-        PORTB|=(1<<DDI_B);
-        _delay_ms(500);
-        PORTB|=(1<<DDI_A); //15
-        _delay_ms(500);*/
-		
     }
 }
-
-    
